11bccc6d87d2d232f90d7220d499ea788372c912
Olivier Larchevêque <olivier.larcheveque@auf.org>
2013-10-07 15:01:28 -0400

    fabric

--- fab/utils.py
+++ fab/utils.py
@@ -7,6 +7,17 @@ from fabric.api import sudo
 
 TPL_DIR = os.path.join(os.path.dirname(__file__), 'tpl')
 
+FABRIC_ENV_PASSWORD = "vagrant"
+
+FABRIC_ENV_ROLEDEFS = {
+    'vagrant': ['vagrant@127.0.0.1:2222', ],
+    }
+
 
 def runcmd(arg):
     sudo("%s" % arg, pty=True)
+
+
+def setup_fabric(env):
+    env.password = FABRIC_ENV_PASSWORD
+    env.roledefs.upadate(FABRIC_ENV_ROLEDEFS)
